ae8cc1899968f12da80d6bb2d59314cad5f185bc
nnapier <nnapier@ggc.edu>
2014-01-07 12:29:38 -0500

    Commit from B1950

---
+++ Sample Project/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author nnapier
+ *
+ */
+public class HelloWorld 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// Nothing really
+		System.out.println("Testing Hello everyone");
+	}
+
+}
