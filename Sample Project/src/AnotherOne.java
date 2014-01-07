edb0c5b8815e062e1395f3cac39328659564eb86
nnapier <nnapier@ggc.edu>
2014-01-07 13:11:37 -0500

    Updates from January 7

---
+++ Sample Project/src/AnotherOne.java
@@ -0,0 +1,28 @@
+/** Class: AnotherOne
+ *  @author Nannette Napier
+ *  @version 1.0
+ *  Course : ITEC 3860 Spring 2014
+ *  Written: Jan 7, 2014
+ *
+ *  This class ---
+ *  Purpose: --
+ */
+
+/**
+ * @author nnapier
+ *
+ */
+public class AnotherOne
+{
+
+	/** Method: main
+	 *  *description*
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
